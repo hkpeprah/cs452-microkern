8acd264600b62055f56c00d92b2318d8c8ec1e1d
hkpeprah <ford.peprah@uwaterloo.ca>
2014-05-25 10:51:50 -0400

    Added tests for create chaining.

--- tests/tasks1.c
+++ tests/tasks1.c
@@ -1,3 +1,6 @@
+/*
+ * Generic tests for finish times and create chaining.
+ */
 #include <utasks.h>
 #include <term.h>
 #include <stdio.h>
@@ -6,7 +9,9 @@
 #include <kernel.h>
 #include <syscall.h>
 
-static int finishTimes[8];
+#define NUM_TASKS  12
+
+static int finishTimes[NUM_TASKS];
 static int finishIndex;
 
 
@@ -27,23 +32,42 @@ void task2() {
 }
 
 
+void task3() {
+    uint32_t tid;
+    tid = MyTid();
+    if (tid < NUM_TASKS - 1) {
+        Create(1, &task3);
+        Pass();
+    }
+    finishTimes[finishIndex++] = tid;
+    Exit();
+}
+
+
 int main() {
     int status;
+    void (*code)();
     unsigned int i;
+    unsigned int len;
     uint32_t tid;
-    int priorities[] = {2, 1, 15, 6, 7, 1, 1, 3};
-    int expected[] = {2, 4, 3, 7, 0, 5, 6, 1};
+    int priorities[] = {2, 1, 15, 6, 7, 1, 1, 3, 1};
+    int expected[] = {2, 4, 3, 7, 0, 5, 6, 1, 8, 9, 11, 10};
 
     finishIndex = 0;
+    len = 9;
     boot();
 
     /* test for task creation */
-    for (i = 0; i < 8; ++i) {
+    for (i = 0; i < len; ++i) {
         if (i < 4) {
-            status = sys_create(priorities[i], task2, &tid);
-        } else if (i >= 4) {
-            status = sys_create(priorities[i], task1, &tid);
+            code = task2;
+        } else if (i >= 4 && i < len - 1) {
+            code = task1;
+        } else {
+            code = task3;
         }
+
+        status = sys_create(priorities[i], code, &tid);
 
         if (status != 0) {
             printf("Tests Failed: Failed on task %d, could not create tasks.\r\n", i);
@@ -54,13 +78,13 @@ int main() {
     /* test task scheduling */
     kernel_main();
 
-    for (i = 0; i < 8; ++i) {
+    for (i = 0; i < NUM_TASKS; ++i) {
         if (finishTimes[i] != expected[i]) {
             puts("Test scheduling failed.\r\nExpected:");
-            for (i = 0; i < 8; ++i) printf(" %d", expected[i]);
+            for (i = 0; i < NUM_TASKS; ++i) printf(" %d", expected[i]);
             newline();
             puts("Actual: ");
-            for (i = 0; i < 8; ++i) printf(" %d", finishTimes[i]);
+            for (i = 0; i < NUM_TASKS; ++i) printf(" %d", finishTimes[i]);
             return 1;
         }
     }
