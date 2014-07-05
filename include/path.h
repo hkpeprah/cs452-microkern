cea6a40503daa7364bcf3cd4e9a426c9c371b637
hkpeprah <ford.peprah@uwaterloo.ca>
2014-07-05 18:44:09 -0400

    Placeholders for path finding

---
+++ include/path.h
@@ -0,0 +1,7 @@
+#ifndef __PATH_H__
+#define __PATH_H__
+#include <track_node.h>
+#include <track_data.h>
+
+
+#endif /* __PATH_H__ */
---
+++ src/path.h
@@ -0,0 +1,10 @@
+/*
+ * path.c - pathfinding algorithms on the train track
+ * implementation details:
+ *
+ */
+
+/* TODO: Bi-directional Djikstra's */
+/* TODO: Turn node array into a grid */
+/* TODO: Weighted edges */
+/* TODO: Figure out how to implement this with synchronization */
